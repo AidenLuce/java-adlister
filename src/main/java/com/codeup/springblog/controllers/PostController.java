cf41c075dbd6a94a6f1a14a5984a73c21aa6a714
Aiden Luce <luceaiden4124@gmail.com>
2022-11-18 15:15:04 -0600

    posts app

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,33 +1,46 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.modals.post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/posts")
 public class PostController {
 
-    @GetMapping
-    @ResponseBody
-    public String showAll(){
-        return "posts index page";
+    @GetMapping ("/index")
+    public String allPosts(Model model){
+        post post1 = new post(1,"First","this is the body");
+        post post2 = new post(2,"Second","this is the body");
+        List<post>  allPosts = new ArrayList<>(List.of(post1, post2));
+        model.addAttribute("allPosts",allPosts);
+        return "index";
     }
 
-    @GetMapping ("/{id}")
-    @ResponseBody
-    public String viewById(@PathVariable String id){
-        return "view an individual post";
+    @GetMapping ("/show/{id}")
+    public String viewById(@PathVariable long id, Model model){
+        post post1 = new post(1,"First","this is the body");
+        post post2 = new post(2,"Second","this is the body");
+        post post3 = new post(3,"yo","wassup?");
+        List<post> allPosts = new ArrayList<>(List.of(post1, post2,post3));
+        model.addAttribute("postId", id);
+        post post = null;
+        for (post userPost : allPosts){
+            if(userPost.getId() == id){
+                post = userPost;
+            }
+        }
+        model.addAttribute("post", post);
+        return "/show";
     }
 
     @GetMapping ("/create")
     @ResponseBody
-    public String viewById(){
-        return "view the form for creating a post";
-    }
-
-    @RequestMapping (path ="/create", method = RequestMethod.POST)
-    @ResponseBody
     public String create(){
-        return "create a new post";
+        return "view the form for creating a post";
     }
 }
---
+++ src/main/java/com/codeup/springblog/modals/post.java
@@ -0,0 +1,48 @@
+package com.codeup.springblog.modals;
+
+public class post {
+
+    private long id;
+    private String title;
+    private String body;
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    public post (){}
+
+    public post(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+    public post (long id, String title, String body){
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+}
+
+
+//
+
