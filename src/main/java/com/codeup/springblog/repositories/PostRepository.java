b3a8080458e053961c5e010e15ef3e90af3e11fc
Aiden Luce <luceaiden4124@gmail.com>
2022-11-18 16:31:20 -0600

    restart

--- src/main/java/com/codeup/springblog/repositories/PostRepository.java
+++ src/main/java/com/codeup/springblog/repositories/PostRepository.java
@@ -1,5 +1,5 @@
 package com.codeup.springblog.repositories;
 import com.codeup.springblog.modals.post;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 public interface PostRepository extends JpaRepository<post, Long>{
 }
